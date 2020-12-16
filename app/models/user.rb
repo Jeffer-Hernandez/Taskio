37cbe2ecfc98e6ddd89b6bdb3fd40cd5ca0a82c7
github username <jhernandez1692@gmail.com>
2020-12-16 12:41:41 -0500

    added associations for user model.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,9 @@
 class User < ApplicationRecord
+
+    has_many :projects
+    has_many :tasks
+    has_many :comments
+    has_many :commented_tasks, through :comments, source: :task
+    has_many :tasks, through :project
+
 end
