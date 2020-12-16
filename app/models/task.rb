a9fcd78abe0914ebb53eea9e867c4750fda371d6
github username <jhernandez1692@gmail.com>
2020-12-16 13:55:32 -0500

    tasks belong to users and projects.

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,9 +1,10 @@
 class Task < ApplicationRecord
 
   belongs_to :project
+  belongs_to :user
 
   has_many :comments
-  has_many :users, through: :projects
-  has_many :comments, through: :users
+  has_many :users, through: :projects, source: :user
+ 
 
 end
