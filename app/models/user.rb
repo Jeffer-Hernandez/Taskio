4a4dadbea26053657f594995b9182c26888f5144
github username <jhernandez1692@gmail.com>
2020-12-16 14:55:12 -0500

    deleted has many tasks through projects.

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,6 @@ class User < ApplicationRecord
     has_many :tasks
     has_many :comments
     has_many :commented_tasks, through: :comments, source: :task
-    has_many :tasks, through: :project
+    
 
 end
