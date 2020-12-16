99fec0283e8ce47a717aab2a5e1714634e34d5da
github username <jhernandez1692@gmail.com>
2020-12-16 13:07:19 -0500

    edited user model for "though" relationship.

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
     has_many :projects
     has_many :tasks
     has_many :comments
-    has_many :commented_tasks, through :comments, source: :task
-    has_many :tasks, through :project
+    has_many :commented_tasks, through: :comments, source: :task
+    has_many :tasks, through: :project
 
 end
