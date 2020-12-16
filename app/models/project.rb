efc398c6dc1106adccfbdf8e057774a5aee1568e
github username <jhernandez1692@gmail.com>
2020-12-16 14:54:13 -0500

    ammended has man through tasks to include source.

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,9 +1,9 @@
 class Project < ApplicationRecord
 
     belongs_to :user
-    
+
     has_many :tasks
-    has_many :users, through: :tasks
+    has_many :users, through: :tasks, source: :user
     has_many :comments, through: :tasks
 
 end
